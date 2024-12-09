b077b75cc8b93bfed9b9ab16565bbb2c54eb4cb4
Gerves Francois  BANIAKINA <123437880+GBANIAKI@users.noreply.github.com>
2024-12-09 05:07:14 +0000

    init commit

--- src/Ch01-Arrays/01_05/begin/find-primes.cpp
+++ src/Ch01-Arrays/01_05/begin/find-primes.cpp
@@ -2,10 +2,28 @@
 #include <vector>
 
 using namespace std;
+bool isPrime(int n){
+
+     if (n<2){
+        return false;
+     }
+     for(int i=2; i*i <=n; ++i){
+         if(n%i==0){
+             return false;
+         }
+     }
+     return true;
+}
 
 void findPrimes(const int arr[], size_t size, std::vector<int> &primes)
 {
     // TODO: Implement
+    for(size_t i=0; i< size; ++i){
+        if(isPrime(arr[i])){
+            primes.push_back(arr[i])
+        }
+    }
+    
 }
 
 int main()
